961dbd5e5c526203d7bd2f7c37e48aee75139f7d
Khoi1308 <duongminhanhkhoi13082002@gmail.com>
2023-02-12 22:58:09 +0700

    first commit

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+console.log(4+5+6);
+
+var lastName = "DuongMinhAnhKhoi";
+console.log(lastName);
